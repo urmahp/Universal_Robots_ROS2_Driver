e83382158254355bdd60e62f34395467b56af2e6
mahp <mahp@universal-robots.com>
2021-06-23 09:56:51 +0200

    Reorder github action

--- .github/dockerursim/build_and_run_docker_ursim.sh
+++ .github/dockerursim/build_and_run_docker_ursim.sh
@@ -10,9 +10,6 @@ docker run --name="mydockerursim" -d \
   -e ROBOT_MODEL=UR5 \
   --net static_test_net \
   --ip 192.168.56.101 \
-  -p 8080:8080 \
-  -p 29999:29999 \
-  -p 30001-30004:30001-30004 \
   -v "${DIR}/.vol":/ursim/programs \
   -v dockursim:/ursim \
   --privileged \
